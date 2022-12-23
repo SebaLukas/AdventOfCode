997cc3282161c99b156931865b7c64bfcd82bf54
Sebastian Lukas <sebastian.lukas@pionix.de>
2022-12-23 10:05:44 +0100

    Solution for 2015 Day 1 part 2 (C++)
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

--- 2015/Day1/main.cpp
+++ 2015/Day1/main.cpp
@@ -3,13 +3,10 @@
 #include <fstream>
 #include <streambuf>
 
-int main ()
-{
+int part1(std::string input) {
     int platform = 0;
-    std::ifstream t("input.txt");
-    std::string s_input((std::istreambuf_iterator<char>(t)),
-                    std::istreambuf_iterator<char>());
-    for(char& c : s_input) {
+
+    for(char& c : input) {
         if (c == '(') {
             platform++;
         } else if (c == ')') {
@@ -17,5 +14,39 @@ int main ()
         }
     }
 
-    std::cout << "Platform: " << platform;
+    return platform;
+}
+
+int part2(std::string input) {
+    int platform = 0;
+    int position = 0;
+
+    for(char& c : input) {
+        position++;
+        if (c == '(') {
+            platform++;
+        } else if (c == ')') {
+            platform--;
+        }
+        
+        if (platform == -1) {
+            return position;
+        }
+    }
+
+    return position;
+}
+
+int main ()
+{
+    
+    std::ifstream t("input.txt");
+    std::string s_input((std::istreambuf_iterator<char>(t)),
+                    std::istreambuf_iterator<char>());
+    
+    // int floor = part1(s_input);
+    // std::cout << "Platform: " << floor;
+
+    int position = part2(s_input);
+    std::cout << "Position: " << position;
 }
